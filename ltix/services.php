10ecbbeda360d6a8a59482ab453c329500ef684d
ismael-texidor <irodriguez@turnitin.com>
2023-11-14 16:17:38 -0500

    MDL-79596 core_ltix: promote ltiservice toolproxy to core

--- ltix/services.php
+++ ltix/services.php
@@ -15,9 +15,15 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+<<<<<<<< HEAD:ltix/services.php
  * This file contains a controller for receiving LTI service requests
  *
  * @package    core_ltix
+========
+ * Strings for component 'ltixservice_toolproxy', language 'en'
+ *
+ * @package    ltixservice_toolproxy
+>>>>>>>> 4d7aacb583d (MDL-79596 core_ltix: promote ltiservice toolproxy to core):ltix/service/toolproxy/lang/en/ltixservice_toolproxy.php
  * @copyright  2014 Vital Source Technologies http://vitalsource.com
  * @author     Stephen Vickers
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
