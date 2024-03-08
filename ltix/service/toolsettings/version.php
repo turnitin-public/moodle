4192e0b348da79046ad06ee4adc7c86aa0fc0200
Dan Marsden <dan@danmarsden.com>
2024-03-08 16:15:00 +1300

    MDL-79597 core_ltix: promote ltiservce toolsettings to core

--- ltix/service/toolsettings/version.php
+++ ltix/service/toolsettings/version.php
@@ -15,17 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'ltiservice_toolsettings', language 'en'
+ * Version information for the ltixservice_toolsettings service.
  *
- * @package    ltiservice_toolsettings
+ * @package    ltixservice_toolsettings
  * @copyright  2014 Vital Source Technologies http://vitalsource.com
  * @author     Stephen Vickers
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['allow'] = 'Use this service';
-$string['ltiservice_toolsettings'] = 'Tool Settings';
-$string['ltiservice_toolsettings_help'] = 'Allow the tool to save and retrieve setting values.';
-$string['notallow'] = 'Do not use this service';
-$string['pluginname'] = 'Tool Settings Service';
-$string['privacy:metadata'] = 'The Tool Settings Service plugin does not store any personal data.';
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$plugin->version   = 2023100900;
+$plugin->requires  = 2023100400;
+$plugin->component = 'ltixservice_toolsettings';
+$plugin->dependencies = [
+    'ltixservice_profile' => 2023100400,
+    'ltixservice_toolproxy' => 2023100400,
+];
